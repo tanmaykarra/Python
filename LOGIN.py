37e609e7857aed24db0ba242dda3ac49f6cb9f63
tanmaykarra <156395707+tanmaykarra@users.noreply.github.com>
2024-06-10 19:10:22 +0530

    Update LOGIN.py

--- LOGIN.py
+++ LOGIN.py
@@ -1,75 +1,151 @@
+#import starts
 import curses
 import os
 import sys
 from colorama import Fore, Back, Style 
-import emoji 
-import speedtest 
-st = speedtest.Speedtest() 
-print("THIS IS YOUR INTERNET SPEED",st.download())   
+# import emoji 
+import smtplib, ssl
+import random
+import speedtest
+import mysql.connector
+#import ends
+
+# #connection to the database
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="tanmay",
+#   password="tanmaykarra"
+# )
+
+
+#clear starts
 def clear():
   curses.setupterm()
   e3 = curses.tigetstr('E3') or b''
   clear_screen_seq = curses.tigetstr('clear') or b''
   os.write(sys.stdout.fileno(), e3 + clear_screen_seq)
+#clear ends
+
+#heading start
+heading = "Hello! Welcome to HtGRAm WEBSITE!"
+x = heading.title()
+print(Fore.LIGHTWHITE_EX + Style.BRIGHT + x)
+#heading end
+
+print(Fore.RED+ "\n PICK ANY ONE FROM ABOVE:")
 
 acc = {}
+
 while True:
     print(Style.BRIGHT+ '')
-    print(Fore.GREEN + '1. Make a new account')
-    print("2. Log in")
-    print("3. Delete a account")
+    print(Fore.GREEN + '1. Register a New Account')
+    print("2. Log in your Account")
+    print("3. Delete an Account")
+    print("4. Check your Internet Speed")
+    print("5. Forgot passoword")
 
-    ans = int(input(Fore.LIGHTWHITE_EX +"enter your choice:"))
+    ans = int(input(Fore.LIGHTWHITE_EX +"\n Enter your choice:"))
     clear()
+    
 #new account starts
     if (ans == 1):
-        x = input(Fore.BLUE +"enter a username:")
+        x = input(Fore.BLUE +"Enter your username:")
         if (acc.get(x)) == None:
-            y = input("enter a password:")
+            y = input("Enter your password:")
             acc[x]=y
         else:
-            print("you already have a account please login")
+            print(Fore.RED+ "ACCOUNT ALREADY REGISTERED")
+            break
+        more = input("\n Do you want to go on?(Yes/No)")
+        if (more=='Yes'):
+            print("\n Here you go")
+        else:
             break
         clear()   
 #new account end
+
 #login start
     elif (ans == 2):
-        u = input(Fore.CYAN + "enter the username:")
+        u = input(Fore.CYAN + "Enter your username:")
         if (acc.get(u)) == None:
-            print("your account was not found")
+            print("ACCOUNT NOT REGISTERED")
             break
         else:    
-            p = input("enter the password:")
+            p = input("Enter the password:")
         def get_key(val):
             for key, value in acc.items():
                 if val == value:
                     return key
         if (get_key(p))==None:
-                print("wrong username or password")
+                print(Fore.RED + "\n Wrong Username or Password")
                 break
         else:    
-                print("logged in")
+                print("\n Logged in")
                 print(emoji.emojize(":grinning_face_with_big_eyes:"))  
-        more = input("do you want to go on?(Yes/No)")
-        if (more=='Yes'):
-            print("here we go")
+        more_ = input("\n Do you want to go on?(Yes/No)")
+        if (more_=='Yes'):
+            print("\n Here you go")
         else:
             break
         clear()   
 #login ends
+
 #delete account starts
     elif (ans == 3):
-        de = input(Fore.LIGHTMAGENTA_EX + "enter your username:")
-        sure=input("are you sure you want to delete your account?(Yes/No)")
+        de = input(Fore.LIGHTMAGENTA_EX + "Enter your Username:")
+        sure=input("\n Are you sure you want to delete your account?(Yes/No)")
         if sure=='Yes':
             acc.pop(de)
             print("Your account is deleted")
         else:
             print("your account is intact")
+        _less = input(Fore.RED +"\n Do you want to go on?(Yes/No)")
+        if (_less=='Yes'):
+            print("\n Here we go")
+        else:
+            break  
         clear()     
 #delete account ends
-#Invalid option            
+
+    #speedtest start
+    elif (ans == 4):
+        st = speedtest.Speedtest() 
+        print("THIS IS YOUR INTERNET SPEED",st.download()) 
+        more_less = input(Fore.RED +"\n Do you want to go on?(Yes/No)")
+        if (more_less=='Yes'):
+            print("\n Here we go")
+        else:
+            break  
+        clear()
+#speedtest ends
+
+#otp starts
+    elif (ans==5):
+        receiver_email = input("Enter your email address:")
+        s = smtplib.SMTP("smtp.gmail.com" , 587) 
+        s.starttls()
+        s.login("mailpython123q@gmail.com" , "vwbaijougcqvmyto")
+        otp = random.randint(1000, 9999)
+        otp = str(otp)
+        s.sendmail("sender_email", receiver_email, otp)
+        print("\n OTP sent succesfully...")
+        check = input("\n Enter the OTP:")
+        if (check==otp):
+            find = input("Enter your  Username:")
+            print(acc.get(find))
+            if acc.get(find)==None:
+                print (Fore.RED +"\n SORRY,NO ACCOUNT FOUND")
+            
+        else:
+            print(Fore.RED +"\n Wrong otp")
+        s.quit() 
+        more_ = input(Fore.RED +"\n Do you want to go on?(Yes/No)")
+        if (more_=='Yes'):
+            print("\n Here we go")
+        else:
+            break  
+#otp ends
     else:
-        print(Fore.RED +"invalid  option")
-    clear()     
+        print(Fore.RED +"\n Invalid  option")
+    clear()
 #Invalid option ends
